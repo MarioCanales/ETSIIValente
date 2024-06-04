c77e42f1cd216c86be3a28136c2fef2768325d53
Mario Canales Torres <mariocanalestorres@MacBook-Pro-de-Mario.local>
2024-06-04 20:13:24 +0200

    Fix format issue

--- lib/utils/circuitUtils.dart
+++ lib/utils/circuitUtils.dart
@@ -18,7 +18,7 @@ class CircuitUtils {
   }
 
   static String formatOhms(double resistance) {
-    if (resistance < 100) {
+    if (resistance.abs() < 100) {
       return "${resistance.toStringAsFixed(2)} Ω";
     } else {
       return "${(resistance / 1000).toStringAsFixed(2)} kΩ";
@@ -26,7 +26,7 @@ class CircuitUtils {
   }
 
   static String formatAmperes(double current) {
-    if (current < 1) {
+    if (current.abs() < 1) {
       return "${(current * 1000).toStringAsFixed(2)} mA";
     } else {
       return "${current.toStringAsFixed(2)} A";
@@ -34,7 +34,7 @@ class CircuitUtils {
   }
 
   static String formatVolts(double voltage) {
-    if (voltage < 0.1) {
+    if (voltage.abs() < 0.1) {
       return "${(voltage * 1000).toStringAsFixed(2)} mV";
     } else {
       return "${voltage.toStringAsFixed(2)} V";
